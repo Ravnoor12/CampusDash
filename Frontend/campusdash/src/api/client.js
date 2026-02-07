226fa6a160d410550e1d8c4946c7098185762e1f
Ravnoor Singh <ravnoor12@gmail.com>
2026-02-07 14:23:17 -0500

    changed the base api path for docker

--- Frontend/campusdash/src/api/client.js
+++ Frontend/campusdash/src/api/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const client = axios.create({baseURL: 'http://10.16.246.191:5091'});
+const client = axios.create({baseURL: 'http://backend:5091'});
 
 export default client;
