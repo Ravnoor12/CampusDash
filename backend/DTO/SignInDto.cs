5a1c7973dadd9b5c358d04f823af0115ae53d9fe
nerdynoah <wwwgreatdog@gmail.com>
2026-02-07 13:47:24 -0500

    Fixed

--- backend/DTO/SignInDto.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-public class SignInDto {
-    [Required(ErrorMessage = "Email is required")]
-    [EmailAddress(ErrorMessage = "Wrong Format")]
-    public string Email {get;set;} = null!;
-    [Required(ErrorMessage = "Password is requrired")]
-    public string Password {get;set;} = null!;
-}
