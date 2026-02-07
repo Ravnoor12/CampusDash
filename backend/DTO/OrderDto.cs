956034ffbdaf2dc0991f4e68235259453e915dc9
nerdynoah <wwwgreatdog@gmail.com>
2026-02-07 10:04:11 -0500

    Revert "Working with order"
    This reverts commit 0880edd73061773248a6abd8787a345100262af4.

--- backend/DTO/OrderDto.cs
+++ backend/DTO/OrderDto.cs
@@ -1,17 +1,21 @@
-namespace CampusDash.Backend.DTO;
-public class OrderDto
-    {
-        public string OrderName { get; set; } = null!; // single order
-        public string CustomerEmail { get; set; } = null!;
-        public string CustomerAddress {get; set; } = string.Empty;
-        public List<string> ShoppingList {get; set;} = new();
-        public double CustomerLat { get; set; }
-        public double CustomerLong { get; set; }
-        public double ShopLat { get; set; }
-        public double ShopLong { get; set; }
-        public List<string> DEmail { get; set; } = new();
-        public List<double> DLat { get; set; } = new();
-        public List<double> DLong { get; set; } = new();
-        public List<string> AssignedEmails { get; set; } = new(); // track assigned delivery persons
-        public string Status { get; set; } = "New";
-    }
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+namespace CampusDash.Backend.Data;
+public class OrderDto {
+    [Required]
+    public string CustomerEmail {get;set;} = null!;
+    [Required]
+    public List<string> DEmail {get;set;} = new();
+    [Required]
+    public double CustomerLat {get;set;}
+    [Required]
+    public double CustomerLong {get;set;}
+    [Required]
+    public List<double> DLat {get;set;} = new();
+    [Required]
+    public List<double> DLong {get;set;} = new();
+    [Required]
+    public double ShopLat {get;set;}
+    [Required]
+    public double ShopLong {get;set;}
+}
